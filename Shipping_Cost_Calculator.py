9efa7352a7833141c7c88c0c2cedbfa044df358b
Bazzinga33 <karim.bazzi95@gmail.com>
2025-01-06 20:51:09 +0100

    Update Shipping_Cost_Calculator.py
    As per coursera project task 3

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,4 +1,5 @@
- # Here is a new update by bazzinga33
+# Here is another update by bazzinga33
+# Here is a new update by bazzinga33
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
